3f60143ff494a9564ac088e3ddc93fe9bcf0b71a
BrennFuchS <pross.patrick@web.de>
2022-01-04 15:55:33 +0100

    Initial commit

---
+++ HipsterSaturn_Bot/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Remora.Commands.Extensions;
+using Remora.Discord.API;
+using Remora.Discord.Commands.Extensions;
+using Remora.Discord.Commands.Services;
+using Remora.Discord.Gateway;
+using Remora.Discord.Gateway.Extensions;
+using Remora.Discord.Gateway.Results;
+using Remora.Discord.Hosting.Extensions;
+using Remora.Discord.Interactivity.Extensions;
+using Remora.Discord.Pagination.Extensions;
+using Remora.Rest.Core;
+using Remora.Results;
+using HipsterSaturn_Bot.Responders;
+
+namespace HipsterSaturn_Bot
+{
+    internal class Program
+    {
+        public static string prefix;
+
+        static async Task Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>()
+                .Build();
+
+            var cancellationSource = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                cancellationSource.Cancel();
+            };
+
+            var botToken = config.GetSection("BOT_TOKEN").Get<string>();
+            prefix = config.GetSection("PREFIX").Get<string>();
+
+            var services = new ServiceCollection()
+                .AddDiscordGateway(_ => botToken)
+                .AddResponder<CommandResponder>()
+                .AddLogging(builder => builder.AddConsole())
+                .BuildServiceProvider();
+
+            var gatewayClient = services.GetRequiredService<DiscordGatewayClient>();
+            var log = services.GetRequiredService<ILogger<Program>>();
+            var runResult = await gatewayClient.RunAsync(cancellationSource.Token);
+
+            if (!runResult.IsSuccess)
+            {
+                switch (runResult.Error)
+                {
+                    case ExceptionError exe:
+                        {
+                            log.LogError
+                            (
+                                exe.Exception,
+                                "Exception during gateway connection: {ExceptionMessage}",
+                                exe.Message
+                            );
+
+                            break;
+                        }
+                    case GatewayWebSocketError:
+                    case GatewayDiscordError:
+                        {
+                            log.LogError("Gateway error: {Message}", runResult.Error.Message);
+                            break;
+                        }
+                    default:
+                        {
+                            log.LogError("Unknown error: {Message}", runResult.Error.Message);
+                            break;
+                        }
+                }
+            }
+
+            Console.WriteLine("Bye bye");
+        }
+    }
+}
---
+++ HipsterSaturn_Bot/Responders/Responders.cs
@@ -0,0 +1,90 @@
+﻿using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.API.Gateway.Events;
+using Remora.Discord.API.Objects;
+using Remora.Discord.Gateway.Responders;
+using Remora.Results;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HipsterSaturn_Bot.Responders
+{
+    public class CommandResponder : IResponder<IMessageCreate>
+    {
+        private readonly IDiscordRestChannelAPI _channelAPI;
+
+        public CommandResponder(IDiscordRestChannelAPI channelAPI)
+        {
+            _channelAPI = channelAPI;
+        }
+
+        public async Task<Result> RespondAsync(IMessageCreate gatewayEvent, CancellationToken ct)
+        {
+            if ((gatewayEvent.Author.IsBot.IsDefined(out var isBot) && isBot) ||
+            (gatewayEvent.Author.IsSystem.IsDefined(out var isSystem) && isSystem) || !gatewayEvent.Content.StartsWith(Program.prefix))
+            {
+                return Result.FromSuccess();
+            }
+
+            var msg = gatewayEvent.Content;
+            var msgC = msg.Split(' ').ToList();
+
+            var pref = msgC[0].Substring(0, Program.prefix.Length);
+            var cmd = msgC[0].Substring(Program.prefix.Length, msgC[0].Length-Program.prefix.Length);
+            msgC.RemoveAt(0);
+
+            if (pref == Program.prefix)
+            {
+                Result<IMessage> replyResult = default;
+
+                switch (cmd)
+                {
+                    case "ping":
+                        {
+                            var embed = new Embed(Description: "Pinging...", Colour: Color.YellowGreen);
+                            var curTime = DateTime.Now.Millisecond;
+
+                            replyResult = await _channelAPI.CreateMessageAsync
+                            (
+                                gatewayEvent.ChannelID,
+                                embeds: new[] { embed },
+                                ct: ct
+                            );
+
+                            curTime = DateTime.Now.Millisecond - curTime;
+
+                            embed = new Embed(Description: $"Pong! {curTime}ms", Colour: Color.Green);
+
+                            await _channelAPI.EditMessageAsync
+                            (
+                                gatewayEvent.ChannelID,
+                                messageID: replyResult.Entity.ID,
+                                embeds: new[] { embed },
+                                ct: ct
+                            );
+
+                            break;
+                        }
+                    case "say":
+                        {
+                            replyResult = await _channelAPI.CreateMessageAsync
+                            (
+                                gatewayEvent.ChannelID,
+                                string.Join(' ', msgC)
+                            );
+                            break;
+                        }
+                }
+                if (!replyResult.IsDefined()) return Result.FromSuccess();
+                else return !replyResult.IsSuccess ? Result.FromError(replyResult) : Result.FromSuccess();
+            }
+            else return Result.FromSuccess();
+        }
+    }
+}
